6fd84e6b5d0dc5e94eee9878dab2c4c56bf377a9
ChloePark85 <hapark85@gmail.com>
2024-04-27 21:49:01 +0900

    added nextauth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MSWComponent } from "./_component/MSWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children, modal }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <MSWComponent />
         {children}
         {modal}
       </body>
