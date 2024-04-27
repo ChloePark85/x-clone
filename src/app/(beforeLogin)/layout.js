5d9f7d6121813b8dcc236d53c532f616fe42ba8d
ChloePark85 <hapark85@gmail.com>
2024-04-27 23:27:02 +0900

    added authSession

--- src/app/(beforeLogin)/layout.js
+++ src/app/(beforeLogin)/layout.js
@@ -1,9 +1,14 @@
+import { ReactNode } from "react";
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
+
 export default function Layout({ children, modal }) {
   return (
-    <div>
-      비포 로그인 레이아웃
+    <div className={styles.container}>
       {children}
       {modal}
     </div>
   );
 }
+
+// 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3001/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
